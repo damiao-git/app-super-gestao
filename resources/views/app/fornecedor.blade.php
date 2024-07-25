52f21457db883f0bcbb887a238cc6ac4bc3e5fea
Damião <jose.faustino@hfa.mil.br>
2024-07-25 08:34:52 -0300

    Login finalizado

---
+++ resources/views/app/fornecedor.blade.php
@@ -0,0 +1,10 @@
+@extends('app.layouts.basico')
+
+@section('title', 'Fornecedor')
+
+@section('content')
+<br>
+<br>
+<br>
+<br>Fornecedor
+@endsection
--- resources/views/app/fornecedor/index.blade.php
+++
@@ -1,9 +0,0 @@
-<h3>Fornecedor</h3>
-
-@if (count($fornecedores) > 0 && count($fornecedores) < 10)
-    <h3>Entre 1 e 9</h3>
-@elseif(count($fornecedores) >= 10)
-    <h3>maior ou igual a 10</h3>
-@else
-    <h3>Nenhum</h3>
-@endif
