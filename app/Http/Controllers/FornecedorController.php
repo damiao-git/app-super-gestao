38cd3f8d45e18e087dc3c6ccb8432e59298bfd85
Damião <jose.faustino@hfa.mil.br>
2024-07-26 10:46:50 -0300

    validacao ao inserir novo fornecedor

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -2,12 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Fornecedor;
 use Illuminate\Http\Request;
 
 class FornecedorController extends Controller
 {
     public function index(){
 
-        return view("app.fornecedor");
+        return view("app.fornecedor.index");
+    }
+    public function listar(){
+        return view("app.fornecedor.listar");
+    }
+    public function adicionar(Request $request){
+
+        $msg = '';
+        if($request->input("_token")){
+            $regras = [
+                "nome" => 'required|min:3|max:40',
+                "site" => 'required',
+                "uf" => 'required|min:2|max:2',
+                "email" => 'email',
+            ];
+            $feedback = [
+                'required' => 'O campo :attribute deve ser preenchido',
+                'nome.min' => 'O campo nome deve ter no minimo 3 caracteres',
+                'nome.max' => 'O campo nome deve ter no maximo 40 caracteres',
+                'uf.min' => 'O campo nome deve ter no minimo 2 caracteres',
+                'uf.max' => 'O campo nome deve ter no maximo 2 caracteres',
+                'email.email' => 'O campo e-mail precisa ser um e-mail válido',
+            ];
+
+            $request->validate($regras, $feedback);
+
+            $fornecedor = new Fornecedor();
+            $fornecedor->create($request->all());
+
+            $msg = 'Cadastro feito com sucesso!';
+        }
+
+        return view("app.fornecedor.adicionar", ["msg"=> $msg]);
     }
 }
