b452e35b5aa6ef64806e2d5abce7bc43cffe00ec
Damião <jose.faustino@hfa.mil.br>
2024-07-29 07:46:35 -0300

    filtrar e listar fornecedores

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -7,17 +7,27 @@ use Illuminate\Http\Request;
 
 class FornecedorController extends Controller
 {
-    public function index(){
+    public function index()
+    {
 
         return view("app.fornecedor.index");
     }
-    public function listar(){
-        return view("app.fornecedor.listar");
+    public function listar(Request $request)
+    {
+
+        $fornecedores = Fornecedor::where('nome', 'like', '%' . $request->input('nome') . '%')
+            ->where('site', 'like', '%' . $request->input('site') . '%')
+            ->where('uf', 'like', '%' . $request->input('uf') . '%')
+            ->where('email', 'like', '%' . $request->input('email') . '%')
+            ->get();
+
+        return view("app.fornecedor.listar", ["fornecedores"=> $fornecedores]);
     }
-    public function adicionar(Request $request){
+    public function adicionar(Request $request)
+    {
 
         $msg = '';
-        if($request->input("_token")){
+        if ($request->input("_token")) {
             $regras = [
                 "nome" => 'required|min:3|max:40',
                 "site" => 'required',
@@ -41,6 +51,6 @@ class FornecedorController extends Controller
             $msg = 'Cadastro feito com sucesso!';
         }
 
-        return view("app.fornecedor.adicionar", ["msg"=> $msg]);
+        return view("app.fornecedor.adicionar", ["msg" => $msg]);
     }
 }
