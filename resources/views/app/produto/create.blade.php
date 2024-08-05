6a4ec23342cb03b55d330b6624f300c241ab6051
Damião <jose.faustino@hfa.mil.br>
2024-08-05 14:54:51 -0300

    criacao component para pagina create e edit produto

--- resources/views/app/produto/create.blade.php
+++ resources/views/app/produto/create.blade.php
@@ -18,27 +18,9 @@
     </div>
     <div class="informacao-pagina">
         <div style="width:30%; margin-left:auto; margin-right: auto;">
-            <form action="{{ route('produto.store')}}" method="post">
-                @csrf
-                <input type="text" name="nome" value="{{ old('nome')}}" placeholder="Nome" class="borda-preta">
-                {{$errors->has('nome') ? $errors->first('nome') : ''}}
+            @component('app.produto._components.form_create_edit', ['unidades' => $unidades])
+            @endcomponent
 
-                <input type="text" name="descricao" value="{{ old('descricao')}}" placeholder="Descrição" class="borda-preta">
-                {{$errors->has('descricao') ? $errors->first('descricao') : ''}}
-
-                <input type="text" name="peso" value="{{ old('peso')}}" placeholder="Peso" class="borda-preta">
-                {{$errors->has('peso') ? $errors->first('peso') : ''}}
-
-                <select name="unidade_id" id="unidade_id">
-                    <option value="">Selecione</option>
-                    @foreach ($unidades as $unidade)
-                        <option value="{{ $unidade->id}}" {{ old('unidade_id') == $unidade->id ? 'selected' : ''}}>{{ $unidade->descricao}}</option>
-                    @endforeach
-                </select>
-                {{$errors->has('unidade_id') ? $errors->first('unidade_id') : ''}}
-
-                <button type="submit" class="borda-preta">Cadastrar</button>
-            </form>
         </div>
     </div>
 </div>
